0fc64857446d82132859600b4c1473eb72f67dda
dammakaru <dammakaru@gmail.com>
2018-05-17 06:59:14 +0530

    using original db

--- src/index.js
+++ src/index.js
@@ -7,14 +7,7 @@ var cors = require('cors');
 
 var app = express();
 
-// mongoose.connect('mongodb://localhost:27017/FoodOrderSystem', err => {
-//   if (err) {
-//     console.warn(err);
-//   }
-//   console.log('database connected');
-// });
-
-mongoose.connect('mongodb://user:pass@ds111885.mlab.com:11885/afdb', err => {
+mongoose.connect('mongodb://localhost:27017/FoodOrderSystem', err => {
   if (err) {
     console.warn(err);
   }
